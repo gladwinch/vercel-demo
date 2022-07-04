39b38987fc08945b01ca1a2466303a3b6fb2bb13
gladwinch <gladwinch20@gmail.com>
2022-07-04 19:08:36 +0530

    api update 12

--- server/index.js
+++ server/index.js
@@ -7,15 +7,21 @@ app.get('/hello', function(req, res) {
     let oldVal = process.env.VERCEL_URL
     process.env.VERCEL_URL = "my new value"
     let newVal = process.env.VERCEL_URL
+
+    let isVercel = false 
+
+    if(process.env.VERCEL === 1) isVercel = true
     res
         .status(200)
-        .json({ success: true, api: 'alex', data: process.env.VERCEL_URL, oldVal, newVal })
+        .json({ success: true, 
+            api: 'alex', 
+            data: process.env.VERCEL_URL, 
+            oldVal, newVal, 
+            isVercels: process.env.VERCEL,
+            isVercel
+        })
         .end()
 })
-
-
-console.log('VERCEL: ------', process.env.VERCEL)
-console.log('VERCEL_URL: ------', process.env.VERCEL_URL)
 
 module.exports = {
     path: '/api',

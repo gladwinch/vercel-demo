907edb5978c9a575bea9c8a142bc6670ebaae379
gladwinch <gladwinch20@gmail.com>
2022-07-04 19:03:29 +0530

    api update 10

--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ const app = express()
 
 app.get('/hello', function(req, res) {
 
+    let oldVal = process.env.VERCEL_URL
+    process.env.VERCEL_URL = "my new value"
+    let newVal = process.env.VERCEL_URL
     res
         .status(200)
-        .json({ success: true, api: 'alex', data: process.env.VERCEL_URL })
+        .json({ success: true, api: 'alex', data: process.env.VERCEL_URL, oldVal, newVal })
         .end()
 })
 
